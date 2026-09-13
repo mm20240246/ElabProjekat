3ab6cf33d063bc086a77889c79d2244d4577a22b
mm20240246 <mm20240246@student.fon.bg.ac.rs>
2026-09-13 12:01:04 +0200

    Add sample event data

---
+++ src/data/events.ts
@@ -0,0 +1,271 @@
+import { EventModel } from "../models/Event";
+
+ 
+
+export const localEvents: EventModel[] = [
+
+  new EventModel(
+
+    "1",
+
+    "Matinee Sunset Party",
+
+    "matinee",
+
+    18,
+
+    "2026-04-12",
+
+    "Beton Hala, Beograd",
+
+    "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=900&q=80",
+
+    "Dnevna žurka uz DJ nastupe, koktele i pogled na reku.",
+
+    1200
+
+  ),
+
+  new EventModel(
+
+    "2",
+
+    "Night Club Energy",
+
+    "nocni-klub",
+
+    21,
+
+    "2026-04-18",
+
+    "Savamala, Beograd",
+
+    "https://images.unsplash.com/photo-1571266028243-d220c9c3a64c?auto=format&fit=crop&w=900&q=80",
+
+    "Noćni provod u klubu sa elektronskom muzikom i specijalnim gostima.",
+
+    1800
+
+  ),
+
+  new EventModel(
+
+    "3",
+
+    "Splav Summer Opening",
+
+    "splav",
+
+    18,
+
+    "2026-05-03",
+
+    "Novi Beograd, Savski kej",
+
+    "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?auto=format&fit=crop&w=900&q=80",
+
+    "Otvaranje sezone na splavu uz popularne domaće i strane hitove.",
+
+    1500
+
+  ),
+
+  new EventModel(
+
+    "4",
+
+    "Kafana Live Night",
+
+    "kafana",
+
+    23,
+
+    "2026-04-25",
+
+    "Skadarlija, Beograd",
+
+    "https://images.unsplash.com/photo-1527529482837-4698179dc6ce?auto=format&fit=crop&w=900&q=80",
+
+    "Veče uživo muzike, tamburaša i tradicionalnog kafanskog provoda.",
+
+    2000
+
+  ),
+
+  new EventModel(
+
+    "5",
+
+    "Matinee Rooftop",
+
+    "matinee",
+
+    21,
+
+    "2026-05-10",
+
+    "Terazije, Beograd",
+
+    "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?auto=format&fit=crop&w=900&q=80",
+
+    "Rooftop matinee žurka sa pogledom na centar grada.",
+
+    1600
+
+  ),
+
+  new EventModel(
+
+    "6",
+
+    "Club Balkan Beats",
+
+    "nocni-klub",
+
+    18,
+
+    "2026-05-15",
+
+    "Dorćol, Beograd",
+
+    "https://images.unsplash.com/photo-1501386761578-eac5c94b800a?auto=format&fit=crop&w=900&q=80",
+
+    "Klupska žurka sa domaćim pop, trap i house zvukom.",
+
+    1400
+
+  ),
+
+  new EventModel(
+
+    "7",
+
+    "Splav White Night",
+
+    "splav",
+
+    23,
+
+    "2026-06-01",
+
+    "Ušće, Beograd",
+
+    "https://images.unsplash.com/photo-1505236858219-8359eb29e329?auto=format&fit=crop&w=900&q=80",
+
+    "Tematska white night žurka na vodi.",
+
+    2200
+
+  ),
+
+  new EventModel(
+
+    "8",
+
+    "Kafana Akustik Veče",
+
+    "kafana",
+
+    18,
+
+    "2026-05-22",
+
+    "Vračar, Beograd",
+
+    "https://images.unsplash.com/photo-1514933651103-005eec06c04b?auto=format&fit=crop&w=900&q=80",
+
+    "Akustično veče za ljubitelje domaće muzike i opuštene atmosfere.",
+
+    1000
+
+  ),
+
+  new EventModel(
+
+    "9",
+
+    "Matinee Pool Party",
+
+    "matinee",
+
+    18,
+
+    "2026-06-08",
+
+    "Ada Ciganlija, Beograd",
+
+    "https://images.unsplash.com/photo-1530103862676-de8c9debad1d?auto=format&fit=crop&w=900&q=80",
+
+    "Letnja dnevna žurka pored bazena sa DJ programom.",
+
+    1700
+
+  ),
+
+  new EventModel(
+
+    "10",
+
+    "Exclusive Club Night",
+
+    "nocni-klub",
+
+    23,
+
+    "2026-06-14",
+
+    "Centar, Beograd",
+
+    "https://images.unsplash.com/photo-1551818255-e6e10975bc17?auto=format&fit=crop&w=900&q=80",
+
+    "Ekskluzivna klupska noć sa ograničenim brojem mesta.",
+
+    2500
+
+  ),
+
+  new EventModel(
+
+    "11",
+
+    "Splav Retro Party",
+
+    "splav",
+
+    21,
+
+    "2026-06-20",
+
+    "Zemunski kej, Beograd",
+
+    "https://images.unsplash.com/photo-1527261834078-9b37d35a4a32?auto=format&fit=crop&w=900&q=80",
+
+    "Retro hitovi na splavu i atmosfera za pamćenje.",
+
+    1300
+
+  ),
+
+  new EventModel(
+
+    "12",
+
+    "Kafana Tamburaši",
+
+    "kafana",
+
+    21,
+
+    "2026-06-28",
+
+    "Skadarlija, Beograd",
+
+    "https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?auto=format&fit=crop&w=900&q=80",
+
+    "Tradicionalno kafansko veče uz tamburaše i domaću hranu.",
+
+    1900
+
+  ),
+
+];
